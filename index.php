1f2d476b94df124ceee12de616c8f7cbf26d7c4e
Sumihiro Ueda <github@sumihiro.biz>
2010-01-12 22:13:27 +0900

    TwitterProxyServerクラス追加

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 // by Sumihiro Ueda http://twitter.com/sumihiro
 
 require_once 'settings.php';
+require_once 'server.php';
 
-
+$server = new TwitterProxyServer($config);
+$server->dispatch();
 
 
 ?>
---
+++ server.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'HTTP/Request.php';
+
+class TwitterProxyServer {
+	var $config;
+	
+	function __construct($config) {
+		$this->config = $config;
+	}
+	
+	function dispatch() {
+		
+	}
+}
+
+
+
+?>
