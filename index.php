3c7f651d6d92b6c878c5cf35ffdcd9c81823642b
Sumihiro Ueda <github@sumihiro.biz>
2010-01-12 22:09:32 +0900

    設定ファイルを追加

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 // Twitterの俺俺プロキシを作るプロジェクト。
 // by Sumihiro Ueda http://twitter.com/sumihiro
 
+require_once 'settings.php';
+
 
 
 
---
+++ settings.sample.php
@@ -0,0 +1,15 @@
+<?php
+// 設定ファイル
+// このファイルを settings.php とリネームし、編集してください。
+
+$config = array(
+	'HTTP_Request' => array(
+		'proxy_host' => 'some host',
+		'proxy_port' => 8000
+	)
+);
+
+
+
+
+?>
